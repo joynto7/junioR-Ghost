845e4ce328dc430bd155e98299fd1292a71a24fc
joyntoghosh <joyntoghosh7@gmail.com>
2026-03-13 18:00:32 +0600

    add ASNI colorclass

--- autorecon.py
+++ autorecon.py
@@ -14,3 +14,12 @@ import os
 import json
 import datetime
 from pathlib import Path
+class C:
+    RED    = "\033[91m"
+    GREEN  = "\033[92m"
+    YELLOW = "\033[93m"
+    CYAN   = "\033[96m"
+    BLUE   = "\033[94m"
+    BOLD   = "\033[1m"
+    DIM    = "\033[2m"
+    RESET  = "\033[0m"
